b4e537f3512274b6a70d0c9e713f3d7fe89eb279
zhaoyongqiang <zhyq0826@gmail.com>
2022-01-09 08:11:44 +0800

    add map

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -3,14 +3,14 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class RentalImageComponent extends Component {
-    // constructor(...args) {
-    //     super(...args);
-    //     this.isLarge = false;
-    // }
+  // constructor(...args) {
+  //     super(...args);
+  //     this.isLarge = false;
+  // }
 
-    @tracked isLarge = false;
-    
-    @action toggleSize() {
-        this.isLarge = !this.isLarge;
-      }
+  @tracked isLarge = false;
+
+  @action toggleSize() {
+    this.isLarge = !this.isLarge;
+  }
 }
--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZW1iZXJqcyIsImEiOiJjazBycmxldGwwMDVvM2VxajdkYjl1OHY0In0.YeeBOuhKFJmuL3Tt9ueMgg';
+
   return ENV;
 };
