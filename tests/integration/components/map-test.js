c21e247105c3ed66c08c892372e86daf72238653
zhaoyongqiang <zhyq0826@gmail.com>
2022-01-09 08:31:02 +0800

    map add getter

--- tests/integration/components/map-test.js
+++ tests/integration/components/map-test.js
@@ -18,12 +18,12 @@ module('Integration | Component | map', function (hooks) {
     />`);
 
     assert
-    .dom('.map img')
-    .exists()
-    .hasAttribute('alt', 'Map image at coordinates 37.7797,-122.4184')
-    .hasAttribute('src')
-    .hasAttribute('width', '150')
-    .hasAttribute('height', '120');
+      .dom('.map img')
+      .exists()
+      .hasAttribute('alt', 'Map image at coordinates 37.7797,-122.4184')
+      .hasAttribute('src')
+      .hasAttribute('width', '150')
+      .hasAttribute('height', '120');
 
     let { src } = find('.map img');
     let token = encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
@@ -55,5 +55,66 @@ module('Integration | Component | map', function (hooks) {
     />`);
 
     assert.dom('.map img').hasAttribute('alt', 'A map of San Francisco');
+  });
+
+  test('it updates the `src` attribute when the arguments change', async function (assert) {
+    this.setProperties({
+      lat: 37.7749,
+      lng: -122.4194,
+      zoom: 10,
+      width: 150,
+      height: 120,
+    });
+
+    await render(hbs`<Map
+      @lat={{this.lat}}
+      @lng={{this.lng}}
+      @zoom={{this.zoom}}
+      @width={{this.width}}
+      @height={{this.height}}
+    />`);
+
+    let img = find('.map img');
+
+    assert.ok(
+      img.src.includes('-122.4194,37.7749,10'),
+      'the src should include the lng,lat,zoom parameter'
+    );
+
+    assert.ok(
+      img.src.includes('150x120@2x'),
+      'the src should include the width,height and @2x parameter'
+    );
+
+    this.setProperties({
+      width: 300,
+      height: 200,
+      zoom: 12,
+    });
+
+    assert.ok(
+      img.src.includes('-122.4194,37.7749,12'),
+      'the src should include the lng,lat,zoom parameter'
+    );
+
+    assert.ok(
+      img.src.includes('300x200@2x'),
+      'the src should include the width,height and @2x parameter'
+    );
+
+    this.setProperties({
+      lat: 47.6062,
+      lng: -122.3321,
+    });
+
+    assert.ok(
+      img.src.includes('-122.3321,47.6062,12'),
+      'the src should include the lng,lat,zoom parameter'
+    );
+
+    assert.ok(
+      img.src.includes('300x200@2x'),
+      'the src should include the width,height and @2x parameter'
+    );
   });
 });
